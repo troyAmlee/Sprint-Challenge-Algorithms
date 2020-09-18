280e1ce429374bee76a44a1e00470e8852296df5
troyAmlee <troyamlee19@gmail.com>
2020-09-18 10:52:50 -0500

    [mvp]recursive count works

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,28 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+wordup = "thissymbolsthissymbol"
+
 def count_th(word):
+    index1 = 0
+    index2 = 1
+    count = 0
+    def wordcount(word, count = 0):
+        if(len(word) == 0):
+            print(count)
+            return count
+        elif(word[index1:index2 + 1] == "th"):
+            print(word[index1:index2 + 1])
+            count += 1
+            nextword = word[index2:]
+            print(nextword)
+            wordcount(nextword, count)
+        else:
+            nextword = word[index2:]
+            print(nextword)
+            wordcount(nextword, count)
+    wordcount(word, count)
     
-    # TBC
-    
-    pass
+
+count_th(wordup)
